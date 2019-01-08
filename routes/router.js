e5129457b4066d1191266d7ad73c7ab7113150e3
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-08 11:48:08 -0500

    added cards for tarot

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,25 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cards = require('../models/cards');
 
+function get_random_cards(){
+    /* returns a random cards */
+    super_cards = [3];
+    ix = [3];
+    for(i=0; i<=2; i++){
+        do{
+            index = Math.floor(Math.random() * cards.length);
+        }while(ix.includes(index));
+        super_cards.push({name: cards[index].name, meaning: cards[index].meaning[i]});
+        ix.push(index);
+    }
+    return super_cards;
+}
 
 router.get('/', (req, res, next) => {
-    x = {
-        'nombre': 'manuel',
-        'apellido': 'fernandez'
-    }
+    card = get_random_cards();
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(x));
+    res.send(JSON.stringify(card));
 });
 
 module.exports = router;
