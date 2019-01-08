d86f5f77f6a42bc212744747a42526fc187d3888
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-08 14:32:31 -0500

    small typos fixed

--- models/cards.js
+++ models/cards.js
@@ -1,10 +1,10 @@
 cards = [
     {name: 'fool', meaning: ['you were full of imagination', 'you are in the process of building a new cycle', 'the wisdom of the future is on your grasp']},
     {name: 'magitian', meaning: ['you were a messanger', 'you are in the process of mastering your meaning', 'you are on the path of concious and unconcious process']},
-    {name: 'high priestess', meaning: ['you were in the dream world, full of intints and emotions', 'you are trying to the with your instinctual powers', 'you are full of fertility']},
+    {name: 'high priestess', meaning: ['you were in the dream world, full of instincts and emotions', 'you are trying to the with your instinctual powers', 'you are full of fertility']},
     {name: 'emperor', meaning: ['you were in a position of authority and responsabilties', 'you are full of passion, action and leadership', 'you are on the path of new beginnings']},
     {name: 'hierophant', meaning: ['you were regarded as a spiritual authority', 'you are full of wisdom and energy', 'you are on the path of connecting with a higher self']},
-    {name: 'lovers', meaning: ['you were faced with making an importante choice', 'you are full of balance', 'you are on the path of finding a connections, maybe with another person or with yourself.']},
+    {name: 'lovers', meaning: ['you were faced with making an important choice', 'you are full of balance', 'you are on the path of finding a connections, maybe with another person or with yourself.']},
 ]
 
 module.exports = cards;
--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cards = require('../models/cards');
 
 function get_random_cards(){
-    /* returns a random cards */
+    /* returns random cards */
     var super_cards = [];
     var ix = [];
     for(i=0; i<=2; i++){
@@ -17,9 +17,9 @@ function get_random_cards(){
 }
 
 router.get('/', (req, res, next) => {
-    card = get_random_cards();
+    cards = get_random_cards();
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(card));
+    res.send(JSON.stringify(cards));
 });
 
 module.exports = router;
