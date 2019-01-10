9bb14de2f4955fbf94ee690906a4f627a832148f
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-10 16:27:26 -0500

    added some changes, fix on port

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 let express = require('express');
 let app = express();
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded());
-
-
 // routes
 let router = require('./routes/router.js');
 
 app.use('/', router);
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function(){ console.log(`Corriendo en el puerto ${port}...`) });
