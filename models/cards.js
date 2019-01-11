a7cf6883851fa3e4abfe1ed644cc84a920c9f8dd
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-11 10:15:42 -0500

    added more cards, fixed spelling errors

--- models/cards.js
+++ models/cards.js
@@ -1,10 +1,12 @@
 cards = [
     {name: 'fool', meaning: ['you were full of imagination', 'you are in the process of building a new cycle', 'the wisdom of the future is on your grasp']},
     {name: 'magitian', meaning: ['you were a messanger', 'you are in the process of mastering your meaning', 'you are on the path of concious and unconcious process']},
-    {name: 'high priestess', meaning: ['you were in the dream world, full of instincts and emotions', 'you are trying to the with your instinctual powers', 'you are full of fertility']},
+    {name: 'high priestess', meaning: ['you were in the dream world, full of instincts and emotions', 'you are trying to be in touch with your instinctual powers', 'you are full of fertility']},
     {name: 'emperor', meaning: ['you were in a position of authority and responsabilties', 'you are full of passion, action and leadership', 'you are on the path of new beginnings']},
     {name: 'hierophant', meaning: ['you were regarded as a spiritual authority', 'you are full of wisdom and energy', 'you are on the path of connecting with a higher self']},
-    {name: 'lovers', meaning: ['you were faced with making an important choice', 'you are full of balance', 'you are on the path of finding a connections, maybe with another person or with yourself.']},
+    {name: 'lovers', meaning: ['you were faced with making an important choice', 'you are full of balance', 'you are on the path of finding a connection, maybe with another person or with yourself.']},
+    {name: 'chariot', meaning: ['you miss both an emotional connection and your home.', 'you are full of nurturing, caring and nourishment', 'you are on the path of finding victory.']},
+    {name: 'chariot', meaning: ['you miss both an emotional connection and your home.', 'you are full of inner knowledge', 'you need to be alone, tune in, and listen to your instincts.']},
 ]
 
 module.exports = cards;
